94393e3b03de186fc330554dbabe3687335f1e32
Morishima Takako <tkkhr.0322@outlook.jp>
2022-07-24 20:06:49 +0900

    java練習

---
+++ main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Hello;
+
+/**
+ * @author tkkhr
+ *
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+	}
+
+}
